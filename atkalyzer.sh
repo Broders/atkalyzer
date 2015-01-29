ccb474ae80cd3e9b35432d3ca6752593d741ff5d
Kristian Broders <kristian.broders@gmail.com>
2015-01-29 11:03:24 +0100

    Added a add file option (-f) to read different fail2ban logs

--- atkalyzer.sh
+++ atkalyzer.sh
@@ -3,13 +3,25 @@
 #fail2ban logpath setup
 F2B_LOG_PATH=/var/log/fail2ban.log
 
-#Filter IPs from the fail2ban logfile and stores them in a array
-IP=(`cut -d " " -f 7 ${F2B_LOG_PATH} | sort | uniq`)
+#Set a default log path
+LOG_PATH=$F2B_LOG_PATH
 
-#Find the Geolocation for given IPs and print them with their IP
-for X in "${IP[@]}"
-   do
-     COUNTRY=`geoiplookup ${X} | sed 's/GeoIP Country Edition://g'`
-     echo "${X} $COUNTRY"
+find_countries(){
+ #Filter IPs from the fail2ban logfile and stores them in a array
+ IP=(`cut -d " " -f 7 ${LOG_PATH} | sort | uniq`)
+
+ #Find the Geolocation for given IPs and print them with their IP
+ for X in "${IP[@]}"
+    do
+      COUNTRY=`geoiplookup ${X} | sed 's/GeoIP Country Edition://g'`
+      echo "${X} $COUNTRY"
+ done
+}
+while getopts ':f:' OPTION ; do
+  case "$OPTION" in
+    f) LOG_PATH=${OPTARG};
+  esac
 done
 
+find_countries
+exit $?
